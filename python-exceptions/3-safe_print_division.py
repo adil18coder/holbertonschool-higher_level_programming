15d8b96a67495130433872fa193b73bb94b17aa3
Adil Valiyev <avliyev17@gmail.com>
2025-11-22 22:24:48 +0000

    cvghjk

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,11 @@ def safe_print_division(a, b):
     result = None
     try:
         result = a / b
-        print("Inside result: {:d}".format(int(result)))
     except ZeroDivisionError:
-        print("Inside result: None")
+        result = None
+    finally:
+        if result is not None:
+            print("Inside result: {:d}".format(int(result)))
+        else:
+            print("Inside result: None")
     return result
