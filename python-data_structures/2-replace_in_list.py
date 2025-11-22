9a98aae040e05a1b75b59ccdf3aa4d29bbe1bbe4
Adil Valiyev <avliyev17@gmail.com>
2025-11-22 17:30:00 +0000

    xdfghjkl

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
+def replace_in_list(my_list, idx, new_element):
+    if idx < 0:
         return my_list
-   new_list[idx] = element
-   return new_list
+    
+    if idx >= len(my_list):
+        return my_list
+
+    my_list[idx] = new_element
+    
+    return my_list
