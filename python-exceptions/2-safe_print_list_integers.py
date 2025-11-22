1b15c7ef1215faaeb33334a460df7add616bf611
Adil Valiyev <avliyev17@gmail.com>
2025-11-22 22:14:37 +0000

    sdfghjk

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-def safe_print_division(a, b):
-    try:
-        result = a / b
-    except ZeroDivisionError:
-        result = 0
-    print("Inside result: {:d}".format(int(result)))
-    return result
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError, IndexError):
+            continue
+    print()
+    return count
