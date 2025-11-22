04bb7e6a9c28e7a19c325b536353ca07cc61cf8c
Adil Valiyev <avliyev17@gmail.com>
2025-11-22 21:34:20 +0000

    xcvgbhnjk

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,12 @@
 def roman_to_int(roman_string):
     if type(roman_string) != str or not roman_string:
         return 0
-    r = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+    roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                 'C': 100, 'D': 500, 'M': 1000}
     total = 0
     prev = 0
     for c in reversed(roman_string):
-        value = r[c]
+        value = roman_map[c]
         if value < prev:
             total -= value
         else:
