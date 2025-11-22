a07803af6a402ad1746bab8c41b4082aa1e94380
Adil Valiyev <avliyev17@gmail.com>
2025-11-22 20:41:46 +0000

    dfghjkl

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique = []
+    for x in my_list:
+        if x not in unique:
+            unique.append(x)
+    total = 0
+    for x in unique:
+        total += x
+    return total
