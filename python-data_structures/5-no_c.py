1d2fe929431ecaa7478256ff2a1ffdbd022c1b17
Adil Valiyev <avliyev17@gmail.com>
2025-11-22 18:10:38 +0000

    zxcvfgbhnjk

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string = list(my_string)
-    for i in my_string:
-        if i=="c":
-            i=" "
-    my_string = "".join(my_string)
-    return my_string
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
