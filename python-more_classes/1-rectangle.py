aaa1ad8f16f0e625d4feed860daa592dd5840746
Adil Valiyev <avliyev17@gmail.com>
2025-11-25 09:05:16 +0000

    sdfghjkl

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""askl;lefghjkl"""
+
+
+class Rectangle:
+    """asdfghjklhfd"""
+
+    def __init__(self, width=0, heiht=0):
+        """asdfghjkl;"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """asdfkl;"""
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        """asdfghjkl;"""
+        if not isinstance(value ,int):
+            raise TypeError("you must include an integer type")
+        if value < 0:
+            raise ValueError("you must include positive number")
+        self._width = value
+
+    @property
+    def height(self ,value):
+        """asdfghjkl;"""
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        """asd;lkjhgf"""
+        if not isinstance(value ,int):
+            raise TypeError("you must include an integer type")
+        if value < 0:
+            raise ValueError("you must include positive number")
+        self._width = value
