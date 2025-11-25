d2bdc53003c01fe57487f1a5e12ac56621317685
Adil Valiyev <avliyev17@gmail.com>
2025-11-25 09:12:37 +0000

    dfghjkl

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,39 +1,39 @@
 #!/usr/bin/python3
-"""askl;lefghjkl"""
+"""Defines a Rectangle class with width and height validation."""
 
 
 class Rectangle:
-    """asdfghjklhfd"""
+    """Represents a rectangle with private width and height."""
 
     def __init__(self, width=0, height=0):
-        """asdfghjkl;"""
+        """Initialize rectangle with optional width and height."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """asdfkl;"""
+        """Retrieve the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """asdfghjkl;"""
-        if not isinstance(value ,int):
-            raise TypeError("you must include an integer type")
+        """Set width with type and value validation."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("you must include positive number")
-        self._width = value
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
-        """asdfghjkl;"""
+        """Retrieve the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """asd;lkjhgf"""
-        if not isinstance(value ,int):
-            raise TypeError("you must include an integer type")
+        """Set height with type and value validation."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("you must include positive number")
-        self._width = value
+            raise ValueError("height must be >= 0")
+        self.__height = value
