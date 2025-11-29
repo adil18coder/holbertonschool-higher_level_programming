9ddd54e74232e23b536efb35f5c96df598f9e5ab
Adil Valiyev <avliyev17@gmail.com>
2025-11-29 14:33:59 +0000

    sdfghjkl

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Bu modul lookup funksiyasını ehtiva edir.
+lookup(obj) funksiyası obyektin atribut və metodlarını siyahı şəklində qaytarır.
+"""
+
 def lookup(obj):
     """
     Obyektin mövcud atributlarını və metodlarını siyahı şəklində qaytarır.
