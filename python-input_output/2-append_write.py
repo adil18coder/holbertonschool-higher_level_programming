fcdb8739c673422593c33108caa6de1b959d9756
Adil Valiyev <avliyev17@gmail.com>
2025-11-29 18:43:35 +0000

    sdfghjk

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""UTF-8 faylına mətn əlavə edən modul."""
+
+
+def append_write(filename="", text=""):
+    """UTF-8 faylına mətn əlavə edir və əlavə olunan simvolların sayını qaytarır."""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
