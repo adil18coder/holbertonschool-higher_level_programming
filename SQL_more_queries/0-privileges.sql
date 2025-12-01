d19beb671bb23faa53bfe69f37a37850189c451e
Adil Valiyev <avliyev17@gmail.com>
2025-12-01 10:31:33 +0000

    dfghj

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,3 @@
-#!/bin/bash
-#hello
-for user in user_0d_1 user_0d_2; do
-    echo "Grants for $user@localhost:"
-    mysql -hlocalhost -uroot -p -e "SHOW GRANTS FOR '$user'@'localhost';"
-    echo "------------------------------------"
-done
+-- This script shows grants for two MySQL users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
