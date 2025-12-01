378e84857b921b5a236e3e50ebbccf4092cc72fc
Adil Valiyev <avliyev17@gmail.com>
2025-12-01 10:23:09 +0000

    sdfghjkl

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for user in user_0d_1 user_0d_2; do
+    echo "Grants for $user@localhost:"
+    mysql -hlocalhost -uroot -p -e "SHOW GRANTS FOR '$user'@'localhost';"
+    echo "------------------------------------"
+done
