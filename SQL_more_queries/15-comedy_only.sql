7120b744717b653f32f1fe408637b59fad2c759c
Adil Valiyev <avliyev17@gmail.com>
2025-12-02 10:28:07 +0000

    zxcvbn

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- List all TV shows that belong to the Comedy genre
+SELECT s.title
+FROM tv_shows s
+JOIN tv_show_genres tg ON s.id = tg.show_id
+JOIN tv_genres g ON tg.genre_id = g.id
+WHERE g.name = 'Comedy'
+ORDER BY s.title ASC;
