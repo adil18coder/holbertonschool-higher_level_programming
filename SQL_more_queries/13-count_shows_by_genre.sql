d6ec8d70881496173ece10a75e8bbd814c5f8804
Adil Valiyev <avliyev17@gmail.com>
2025-12-02 08:37:12 +0000

    defdgtevf

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- List all genres with number of shows linked
+SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM genres
+JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+GROUP BY genres.id
+HAVING number_of_shows > 0
+ORDER BY number_of_shows DESC;
