72f1cd9affd196312652e1e4e315e80934cc065d
Adil Valiyev <avliyev17@gmail.com>
2025-12-02 08:24:19 +0000

    fghjkl

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,15 @@
+-- Create database if not exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use this database
+USE hbtn_0d_usa;
+
+-- Create cities table if not exists
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
