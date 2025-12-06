aa13c93ea0bf9369840ac6c827162698c38fba3e
Adil Valiyev <avliyev17@gmail.com>
2025-12-06 19:28:21 +0000

    hcuehbfbh

---
+++ python-network_1/3-code_error.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+3-error_code.py: Fetch a URL and display response body,
+handle HTTPError and print the error code
+"""
+
+from urllib import request, error
+import sys
+
+url = sys.argv[1]
+
+try:
+    # Send the request using with statement
+    with request.urlopen(url) as response:
+        body = response.read()
+        print(body.decode('utf-8'))
+except error.HTTPError as e:
+    print(f"Error code: {e.code}")
