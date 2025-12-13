8c2849ab54a0afbc408a32680941c764e3a84ea4
Adil Valiyev <avliyev17@gmail.com>
2025-12-13 20:53:48 +0000

    ebbeh

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,7 @@
 -- List number of shows per genre
-SELECT genre_id AS genre,
-       COUNT(show_id) AS number_of_shows
-FROM tv_show_genres
-GROUP BY genre_id
+SELECT g.name AS genre, COUNT(s.id) AS number_of_shows
+FROM genres g
+JOIN shows s ON s.genre_id = g.id
+GROUP BY g.id
+HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
