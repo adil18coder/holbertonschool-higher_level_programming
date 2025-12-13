2d701a8334de1d5b3a70a7be722dc29587a5749a
Adil Valiyev <avliyev17@gmail.com>
2025-12-13 21:08:12 +0000

    hbeh

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,13 +1,7 @@
 -- Holberton: TV shows janr və say hesablaması
-USE hbtn_0d_tvshows;
-
-START TRANSACTION;
-
 SELECT g.name AS genre, COUNT(sg.show_id) AS number_of_shows
 FROM tv_genres g
 JOIN tv_show_genres sg ON g.id = sg.genre_id
 GROUP BY g.id
 HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
-
-COMMIT;
